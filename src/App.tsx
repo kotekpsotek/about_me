5882f54674afdc9887a2498fc23f53da7624b2bc
kotekpsotek <siematumichael@protonmail.com>
2023-12-13 16:21:10 +0100

    Main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import UpBar from "./components/UppBar";
 import "./index.css"
+import MainSide from './components/MainSide';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <UpBar/>
+      <MainSide/>
     </>
   )
 }
--- src/components/MainSide.tsx
+++ src/components/MainSide.tsx
@@ -1,13 +1,49 @@
-import profileImg from "../assets/profile-img.jpg"
+import imgProfile from "../assets/profile-img.jpg"
+import { TypeAnimation } from "react-type-animation"
 
 export default function Main() {
+    function setTypoColor(cls: string, previous?: string) {
+        const el = document.getElementsByClassName("sequence-typer")[0];
+        if (previous) el.classList.remove(previous);
+        el.classList.add(cls);
+    }
+    
     return (
         <>
-            <div id="welcome">
-                <img src={profileImg} alt=""/>
-                <div>
-                    
+            <div id="wr" className="w-full h-full flex justify-center items-center px-4 gap-x-8 lg:gap-x-14">
+                <div id="txt" className="text-white">
+                    <p className="text-xs uppercase text-slate-500">Welcome in my world</p>
+                    <h1 className="text-3xl font-bold">Welcome, I'm Michał Szczepański</h1>
+                    {/* <h2 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-sky-800 to-sky-200">JS Developer</h2> */}
+                    <TypeAnimation
+                        sequence={[
+                            // Same substring at the start will only be typed out once, initially
+                            () => setTypoColor("js-typo"),
+                            'JS Developer',
+                            1000, // wait 1s before replacing "Mice" with "Hamsters"
+                            () => setTypoColor("ts-typo"),
+                            'TypeScript Developer',
+                            1000,
+                            () => setTypoColor("rs-typo"),
+                            'Rust Developer',
+                            1000,
+                            () => setTypoColor("py-typo"),
+                            'Python Developer',
+                            1000
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        className="sequence-typer text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-tr"
+                        // style={{ fontSize: '2em', display: 'inline-block' }}
+                        repeat={Infinity}
+                    />
+                    <h3 className="text-lg font-semibold">based in Warsaw, Poland</h3>
+                    <div className="mt-5 flex gap-x-4">
+                        <a className="bg-white rounded text-black p-2">About me</a>
+                        <a className="border border-white rounded text-white p-2">Contact</a>
+                    </div>
                 </div>
+                <img src={imgProfile} alt="" className="hidden md:block h-3/6 w-3/6 lg:w-fit object-contain"/>
             </div>
         </>
     )
